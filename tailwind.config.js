ea00f8bff1e8b1c7174285c6f156dd7530156745
savano <vano_202228@yahoo.com>
2025-02-10 06:14:07 +0700

    chore: fix tailwindcss config preventing build

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts,scss}" // ✅ Ensures Tailwind works inside Angular components & SCSS files
-  ],
-  theme: {},
-};
---
+++ tailwind.configa.js
@@ -0,0 +1,13 @@
+/** 
+ * This file is required by tailwindcss intellisense extension
+ * Temporarily renamed due to throwing an error when building 
+ * the app or running tests.
+ * */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}"
+  ],
+  theme: {},
+};
