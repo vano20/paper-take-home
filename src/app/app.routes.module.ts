9bbe93f783428dffd10a8792f390a5b1971f6022
savano <vano_202228@yahoo.com>
2025-02-10 21:47:28 +0700

    fix: auto redirect to users when entry '/'

--- src/app/app.routes.module.ts
+++ src/app/app.routes.module.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
 ];
