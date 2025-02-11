475ccb2c91f2eb813837c0cf91f605201ee33566
savano <vano_202228@yahoo.com>
2025-02-11 15:59:01 +0700

    test: update user-list test with NgRx store

--- src/app/features/user/components/user-list/user-list.component.spec.ts
+++ src/app/features/user/components/user-list/user-list.component.spec.ts
@@ -1,52 +1,65 @@
 import { ComponentFixture, fakeAsync, TestBed, waitForAsync } from '@angular/core/testing';
 import { UserListComponent } from './user-list.component';
-import { UserService } from '../../service/user.service';
-import { of } from 'rxjs';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
 import { TableModule } from '../../../../shared/components/table/table.module';
 import { Router } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { error, filteredUser, loading } from '../../../../state/user/user.selectors';
+import { loadUsers, setFilter } from '../../../../state/user/user.actions';
+
+const mockUsers = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@example.com',
+    website: 'example.com',
+    url: 'https://example.com',
+    phone: '123-456-7890',
+    address: {
+      street: '123 Main St',
+      suite: 'Apt 1',
+      city: 'Anytown',
+      zipcode: '12345',
+      geo: {
+        lat: '40.7128',
+        lng: '-74.0060',
+      },
+    },
+    company: {
+      name: 'ACME Corp',
+      catchPhrase: 'The best company',
+      bs: 'High level approach on the web',
+    },
+  },
+];
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
-  let mockUserService: jasmine.SpyObj<UserService>;
+  let mockStore: MockStore;
+  let mockRouter: Router;
 
   beforeEach(waitForAsync(() => {
-    mockUserService = jasmine.createSpyObj('UserService', ['getUsers', 'setSearchTerm']);
-
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, FormsModule, TableModule],
+      imports: [RouterTestingModule, FormsModule, TableModule, StoreModule.forRoot({})],
       declarations: [UserListComponent],
-      providers: [
-        { provide: UserService, useValue: mockUserService },
-      ],
+      providers: [provideMockStore()],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
-    mockUserService.filteredUsers$ = of([
-      {
-        id: 1, name: 'John Doe', email: 'john@example.com', website: 'https://example.com', phone: '123-456-7890', address: {
-          street: '123 Main St',
-          suite: 'Apt 1',
-          city: 'Anytown',
-          zipcode: '12345',
-          geo: {
-            lat: '40.7128',
-            lng: '-74.0060'
-          }
-        },
-        company: {
-          name: 'ACME Corp',
-          catchPhrase: 'The best company',
-          bs: 'High level approach on the web'
-        }
-      },
-    ]);
+    mockStore = TestBed.inject(MockStore);
+    mockRouter = TestBed.inject(Router);
+
+    mockStore.overrideSelector(filteredUser, mockUsers);
+    mockStore.overrideSelector(loading, false);
+    mockStore.overrideSelector(error, null);
+
     fixture.detectChanges();
   });
 
@@ -59,17 +72,26 @@ describe('UserListComponent', () => {
     expect(userRows.length).toBe(1);
   });
 
-  it('should call setSearchTerm when search input changes', () => {
+  it('should dispatch setUserFilter when search input changes', () => {
+    spyOn(mockStore, 'dispatch');
+
     const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
     inputElement.value = 'Jane';
     inputElement.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    expect(mockUserService.setSearchTerm).toHaveBeenCalledWith('Jane');
+    expect(mockStore.dispatch).toHaveBeenCalledWith(setFilter({ filter: 'Jane' }));
   });
 
-  it('should call navigate when a row button detail is clicked', fakeAsync(() => {
-    const mockRouter = TestBed.inject(Router);
+  it('should dispatch loadUsers on init', () => {
+    spyOn(mockStore, 'dispatch');
+
+    component.ngOnInit();
+
+    expect(mockStore.dispatch).toHaveBeenCalledWith(loadUsers({ metas: { page: 1, perPage: 10 } }));
+  })
+
+  it('should navigate when a row button detail is clicked', fakeAsync(() => {
     spyOn(mockRouter, 'navigate');
 
     fixture.detectChanges();
